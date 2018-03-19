b035e0aeba529193c99477239e1f70a791c6c4d6
Eric Windmill <ericwindmill@gmail.com>
2018-03-19 07:18:38 -0700

    add Tzolkin tests

--- test/flutter_calendar_test.dart
+++ test/flutter_calendar_test.dart
@@ -2,7 +2,56 @@ import 'package:flutter_calendar/src/utils.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('DateUtil Tests', () {
+  group('Tzolkin Tests', () {
+    test('lastDayInMonth', () {
+      var date = new DateTime(2017, 3);
+      var lastDay = Utils.lastDayOfMonth(date);
+      var expected = new DateTime(2017, 3, 31);
+      expect(lastDay, expected);
+    });
+
+    test('daysInMonth', () {
+      var date = new DateTime(2017, 3);
+      var days = Utils.daysInMonth(date);
+      expect(days, hasLength(35));
+    });
+
+    test('daysInMonthWithTimeChangeFallBack', () {
+      var date = new DateTime(2017, 11);
+      var days = Utils.daysInMonth(date);
+      expect(days, hasLength(35));
+    });
+
+    test('daysInMonthWithTimeChangeSpringForward', () {
+      var date = new DateTime(2018, 4);
+      var days = Utils.daysInMonth(date);
+      expect(days, hasLength(42));
+    });
+
+    test('isSameWeek', () {
+      expect(
+          Utils.isSameWeek(new DateTime(2017, 3, 4), new DateTime(2017, 3, 5)),
+          false);
+      expect(
+          Utils.isSameWeek(new DateTime(2017, 3, 5), new DateTime(2017, 3, 6)),
+          true);
+      expect(
+          Utils.isSameWeek(new DateTime(2017, 2, 26), new DateTime(2017, 3, 4)),
+          true);
+      expect(
+          Utils.isSameWeek(new DateTime(2017, 3, 4), new DateTime(2017, 3, 10)),
+          false);
+      expect(
+          Utils.isSameWeek(new DateTime(2017, 3, 3), new DateTime(2017, 3, 10)),
+          false);
+      expect(
+          Utils.isSameWeek(
+              new DateTime(2017, 3, 10), new DateTime(2017, 3, 10)),
+          true);
+    });
+  });
+
+  group('New Tests', () {
     List<DateTime> testDates = [];
     DateTime today;
 
