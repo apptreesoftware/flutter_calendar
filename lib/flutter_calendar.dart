013025ee3906a8d773b0b31b39a4b63b89670de3
Eric Windmill <eric@ericwindmill.com>
2018-10-08 11:46:41 -0700

    update aspect ratio to fix expansion feature

--- lib/flutter_calendar.dart
+++ lib/flutter_calendar.dart
@@ -120,8 +120,6 @@ class _CalendarState extends State<Calendar> {
         child: new GridView.count(
           shrinkWrap: true,
           crossAxisCount: 7,
-          childAspectRatio: 2.0,
-          mainAxisSpacing: 0.0,
           padding: new EdgeInsets.only(bottom: 0.0),
           children: calendarBuilder(),
         ),
